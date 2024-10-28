53428376006bc9d5985c7c7b8ebed07b5a14dc69
Marcojr05 <marcoantoniojr052005@gmail.com>
2024-10-28 14:20:30 -0300

    prof

--- js/cadastro.js
+++ js/cadastro.js
@@ -4,7 +4,7 @@ function cadastrarUsuario() {
     const password = document.getElementById('password').value;
 
     if (nome && email && password) {
-        // Aqui você pode adicionar a lógica para cadastrar o usuário, como enviar os dados para um servidor
+
         alert(`Usuário cadastrado com sucesso!\nNome: ${nome}\nEmail: ${email}`);
     } else {
         alert('Por favor, preencha todos os campos.');
